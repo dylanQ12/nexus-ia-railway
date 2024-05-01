bd5e37473ee075b6ed9dbf98d8d6fbab676ecbbf
dylanQ12 <136197614+dylanQ12@users.noreply.github.com>
2024-05-01 12:40:23 -0500

    update files

--- nexus/settings.py
+++ nexus/settings.py
@@ -122,6 +122,10 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
