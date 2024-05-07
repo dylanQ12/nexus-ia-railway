e38292ffbc7a28f38e95b0c9f9158aa0d4edcd67
dylanQ12 <136197614+dylanQ12@users.noreply.github.com>
2024-05-07 17:11:31 -0500

    update files

--- nexus/settings.py
+++ nexus/settings.py
@@ -12,13 +12,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d&nn9zfnwxc^7xplmj=5_p^0q5%sn8x36^!nog)v^-^pvo%wtp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-#ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*']
 
-ALLOWED_HOSTS = ['nexus-ia-railway-production.up.railway.app']
+# ALLOWED_HOSTS = ['nexus-ia-railway-production.up.railway.app']
 
-CSRF_TRUSTED_ORIGINS = ['https://nexus-ia-railway-production.up.railway.app']
+# CSRF_TRUSTED_ORIGINS = ['https://nexus-ia-railway-production.up.railway.app']
 
 # Application definition
 
