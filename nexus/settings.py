469ac7d61744315c68717b2fe4c04b94be47e120
dylanQ12 <136197614+dylanQ12@users.noreply.github.com>
2024-05-07 18:51:08 -0500

    update images and section about

--- nexus/settings.py
+++ nexus/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d&nn9zfnwxc^7xplmj=5_p^0q5%sn8x36^!nog)v^-^pvo%wtp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 #ALLOWED_HOSTS = ['*']
 
